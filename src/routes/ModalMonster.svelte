01b94bb2ccbba7c96c8a631c4a82a60f223977a7
LiamKrenn <k.liam1224@gmail.com>
2023-06-23 23:48:39 +0200

    faster image loading, less code, better css

--- src/routes/ModalMonster.svelte
+++ src/routes/ModalMonster.svelte
@@ -4,45 +4,65 @@
 	import { page } from '$app/stores';
 
 	const id: number = $modalStore[0].meta?.id;
-	
+
 	const lang: string = $modalStore[0].meta?.lan;
 	const genId: number = $modalStore[0].meta?.genid;
+
+	$: mon = null;
 
 	async function loadData() {
 		const res = await fetch('/data/pokemon/' + genId + '-' + id + '.json');
 		const json = await res.json();
 		json.id = id;
-		return json;
+		mon = json;
 	}
+
+	loadData();
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 
-<div class="card m-1 cursor-default sm:w-96 w-3/5">
-	<div class="relative sm:text-4xl sm:left-3 sm:top-3 text-xl left-2 top-2 text-surface-300-600-token">
+<div class="card m-1 cursor-default sm:w-[25rem] w-9/12">
+	<div
+		class="relative w-32 text-surface-300-600-token
+		sm:text-4xl sm:left-3 sm:top-3
+		xs:text-3xl left-2 top-2
+		text-xl"
+	>
 		#{id}
 	</div>
-	{#await loadData()}
-	<div class="sm:mx-3 mx-1 justify-center "/>
-	<h1 class="h1 p-2 break-words sm:text-5xl sm:mx-2 sm:mb-4 text-2xl mx-1 text-center text-surface-800-100-token">
-		{langs[lang].loading}
+	<img
+		class="pixelated justify-center sm:w-96 sm:h-96 sm:mx-2 w-[90%] h-[90%] mx-[5%]"
+		src="images/pokemon/{id}.png"
+		alt={mon ? mon.names[lang] : 'unloaded pokemon'}
+		loading="eager"
+	/>
+	<h1
+		class="
+		h1 p-4 break-words text-center text-surface-800-100-token
+		sm:text-5xl
+		xs:text-3xl
+		text-2xl"
+	>
+		{#if !mon}
+			{langs[lang].loading}
+		{:else}
+			{mon.names[lang]}
+		{/if}
 	</h1>
-	<div class="p-2 break-words sm:text-2xl sm:mx-2 sm:mb-4 mx-1 mb-2  text-center text-surface-800-100-token">
-		{langs[lang].loading}
+	<div
+		class="
+		break-words text-center text-surface-800-100-token pb-2
+		sm:text-2xl
+		xs:text-base xs:px-2
+		text-sm px-1"
+	>
+		{#if !mon}
+			{langs[lang].loading}
+		{:else}
+			{mon.descriptions[lang]}
+		{/if}
 	</div>
-	{:then monster}
-	<div class="pixelated  sm:mx-3 mx-1 justify-center " >
-		<img src="images/pokemon/{id}.png"  alt={monster.names[lang]} height="100%" width="100%" loading="eager" />
-
-	</div>
-	<h1 class="h1 p-2 break-words sm:text-5xl sm:mx-2 sm:mb-4 text-2xl mx-1 text-center text-surface-800-100-token">
-		{monster.names[lang]}
-	</h1>
-	<div class="p-2 break-words sm:text-2xl sm:mx-2 sm:mb-4 mx-1 mb-2  text-center text-surface-800-100-token">
-		{monster.descriptions[lang]}
-	</div>
-	{/await}
-	
 </div>
 
 <style>
