cdb002827a2d556b8bbbd1d79e55b1575de77f45
LiamKrenn <k.liam1224@gmail.com>
2023-08-16 13:56:21 +0200

    optimized dom elements

--- src/routes/Monster.svelte
+++ src/routes/Monster.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { langs } from './langs';
+	import { inview } from 'svelte-inview';
 
 	export let id: number;
-	export let monClick: (m: number) => void;
-	import { inview } from 'svelte-inview';
 	export let searchString: string;
-
-	let isInView: boolean;
-	const options = {};
 
 	$: language = $page.url.searchParams.get('lang') || 'en';
 	$: genId = $page.url.searchParams.get('gen-id') || '0';
@@ -22,65 +18,44 @@
 	}
 
 	$: mon = null;
-
-	// langs[language].loading;
-	//async function getName(l: string) {
-	//	const monsterResponse = await fetch(`${monster.url}`);
-	//	const monsterJson = await monsterResponse.json();
-	//	const names = await monsterJson.names;
-	//	name = await names.find((name) => name.language.name == l).name;
-	//	monster.name = name;
-	//}
-	//name = monster.names.en
-
-	//$: tmp = getName(language);
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 
 {#if mon == null || mon['names'][language].toLowerCase().includes(searchString)}
 	<div
-		class="card card-hover cursor-pointer select-none
+		role="button"
+		tabindex="0"
+		class="card card-hover cursor-pointer select-none flex flex-col items-center relative
 			 sm:m-1 sm:w-40 sm:h-[13rem]
 			 m-0.5 w-24 h-[7.7rem]"
-		on:click={() => {
-			console.log(searchString);
-			monClick(id);
-		}}
-		use:inview={options}
-		on:inview_enter={(event) => {
-			const { inView, entry, scrollDirection, observer, node } = event.detail;
-			isInView = inView;
+		on:click
+		use:inview={{}}
+		on:inview_enter={() => {
 			if (mon == null) {
 				loadData();
 			}
 		}}
 	>
-		<div class="relative top-1 left-2 text-surface-300-600-token w-16 sm:text-xl text-sm">
+		<div class="relative top-1 left-0 text-surface-300-600-token w-20 sm:w-36 sm:text-xl text-sm">
 			#{id}
 		</div>
-		<div
-			class="justify-center
-				 sm:mx-2 sm:w-36 sm:h-36
-				 mx-2 w-20 h-20"
-		>
-			{#if !mon}
-				<div
-					class="card animate-pulse variant-soft
-						 sm:h-36 sm:w-36
-						 h-20 w-20"
-				/>
-			{:else}
-				<img
-					class="sm:h-36 sm:w-36
-							h-20 w-20"
-					src="images/pokemon/{mon.id}.png"
-					alt={mon.names[language]}
-					height="96px"
-					width="96px"
-				/>
-			{/if}
-		</div>
+		{#if !mon}
+			<div
+				class="card animate-pulse variant-soft
+						sm:h-36 sm:w-36
+						h-20 w-20"
+			/>
+		{:else}
+			<img
+				class="sm:h-36 sm:w-36
+						h-20 w-20"
+				src="images/pokemon/{mon.id}.png"
+				alt={mon.names[language]}
+				height="96px"
+				width="96px"
+			/>
+		{/if}
 		<div
 			class="text-center text-surface-800-100-token
 				 sm:mx-2 sm:w-36 sm:text-lg
